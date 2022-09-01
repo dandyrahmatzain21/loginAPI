2fcb67784240fded55463be58f4d64d22bb47872
Den <dendizaman@gmail.com>
2022-09-01 12:19:09 +0700

    Menambahkan resource.
    Menambahkan resource seperti image, custom colors, dan firebase authenticationl
    MengubahTarget min sdk di android menjadi sdk 19 dan menambahkan multiindex.

---
+++ lib/res/custom_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color pomegranate = Color(0xFFEF4C29);
+  static final Color signalOrange = Color(0xFFFF7833);
+  static final Color diamondOrange = Color(0xFFF99E3F);
+}
