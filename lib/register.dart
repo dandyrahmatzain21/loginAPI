9c1733631d0f56c99f60ae2de8281263c7824c23
Dandy Zain <83494409+dandyrahmatzain21@users.noreply.github.com>
2022-09-06 01:51:05 +0700

    Update register.dart
    Add Validate

--- lib/register.dart
+++ lib/register.dart
@@ -1,7 +1,8 @@
-import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:survei_asia/login.dart';
 import 'package:survei_asia/services/api_service.dart';
+import 'package:survei_asia/widgets/validator.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   var namaController = TextEditingController();
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
@@ -18,171 +20,216 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: ListView(children: [
-        Image.asset(
-          "images/image1.png",
-          width: 200,
-          height: 70,
-        ),
-        Container(
-          margin: const EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
-          child: const Text(
-            'Selamat Datang',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          children: [
+
+            //Image
+            Image.asset(
+              "images/image1.png",
+              width: 200,
+              height: 70,
             ),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(16.0),
-          child: const Text(
-            'Silakan mendaftarkan akun anda untuk melanjutkan',
-            textAlign: TextAlign.left,
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(16.0),
-          child: const Text(
-            'Nama',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-          child: TextField(
-            controller: namaController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Masukkan Nama Anda',
-            ),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(16.0),
-          child: const Text(
-            'Email',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-          child: TextField(
-            controller: emailController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Masukkan Email Anda',
-            ),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(16.0),
-          child: const Text(
-            'Kata Sandi',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-          child: TextField(
-            obscureText: true,
-            controller: passwordController,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Masukkan Kata Sandi Anda',
-            ),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              ElevatedButton(
-                child: const Text("Daftar"),
-                onPressed: () {
-                  // Aksi ketika button diklik
-                  daftar();
-                },
+
+            //Selamat Datang
+            Container(
+              margin: const EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
+              child: const Text(
+                'Selamat Datang',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
               ),
-            ],
-          ),
+            ),
+
+            //Text
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Silakan mendaftarkan akun anda untuk melanjutkan',
+                textAlign: TextAlign.left,
+              ),
+            ),
+            
+            //FieldNama
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Nama',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: TextFormField(
+                controller: namaController,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Nama Tidak Boleh Kosong";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Masukkan Nama Anda',
+                ),
+              ),
+            ),
+            
+            //FieldEmail
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Email',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  RegExp regex = new RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+                  if (value!.isEmpty) {
+                    return "Email Tidak Boleh Kosong";
+                  } else if (!regex.hasMatch(value!)) {
+                    return "Tolong Isi Email Yang Valid";
+                  }
+                  return null;
+                },
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Masukkan Email Anda',
+                ),
+              ),
+            ),
+
+            //FieldPassword
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Kata Sandi',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Password Tidak Boleh Kosong";
+                  }else if (value!.length < 6) {
+                    return "Password Tidak Boleh Kurang Dari 6";
+                  }
+                  return null;
+                },
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Masukkan Kata Sandi Anda',
+                ),
+              ),
+            ),
+
+            //ButtonDaftar
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ElevatedButton(
+                    child: const Text("Daftar"),
+                    onPressed: () {
+                      // Aksi ketika button diklik
+                        checkValidation(_formKey);
+                        daftar();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ]
         ),
-      ]),
-    ));
+      )
+    );
   }
 
   Future<void> daftar() async {
-    if (namaController.text.isNotEmpty &&
-        emailController.text.isNotEmpty &&
-        passwordController.text.isNotEmpty) {
+    if (namaController.text.isNotEmpty && emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
 
       var email = emailController.text;
       var password = passwordController.text;
       var name = namaController.text;
 
-      if(password.length < 6) {
-        CoolAlert.show(
-          context: context,
-          title: "Password Invalid",
-          type: CoolAlertType.error,
-          text: "Password tidak bisa kurang dari 6 karakter!",
-        );
+      RegExp regex = new RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+
+      //ValidateSnackBar
+      if(password.isEmpty) {
+        ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Password Tidak Boleh Kosong")));
+        return;
+      }else if(password.length <6) {
+        ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Password Tidak Boleh Kurang Dari 6")));
+        return;
+      }else if (email.isEmpty) {
+        ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Email Tidak Boleh Kosong")));
+        return;
+      }else if (!regex.hasMatch(email)) {
+        ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Isi Email Dengan Valid")));
+        return;
+      }else if (password.isEmpty) {
+        ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text("Nama Tidak Boleh Kosong")));
         return;
       }
-
+      
+      //APISERVICE
       print("{nama : $name, \n email : $email, \n password : $password}");
-
       APIService.register(name, email, password).then((response) => {
-            print(response),
-            if (response)
-              {
-                //valid credential
-                CoolAlert.show(
-                    context: context,
-                    title: "Register Berhasil",
-                    type: CoolAlertType.success,
-                    text:
-                        "Akun anda berhasil dibuat, silakan masuk melalui halaman login",
-                    onConfirmBtnTap: () => {
-                          Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginScreen()),
-                              (route) => false)
-                        }),
-                namaController.clear(),
-                emailController.clear(),
-                passwordController.clear()
-              }
-            else
-              {
-                //invalid credential
-                CoolAlert.show(
-                  context: context,
-                  title: "Register Error",
-                  type: CoolAlertType.error,
-                  text:
-                      "Email yang anda masukan sudah terdaftar, silakan gunakan email lain",
-                )
-              }
-          });
-    } else {
+        print(response),
+        if (response){
+          //valid credential
+          ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Register Berhasil"))),
+          Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => const LoginScreen()), (route) => false),
+          namaController.clear(),
+          emailController.clear(),
+          passwordController.clear(),
+        }else{
+          //invalid credential
+          ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Email Yang Digunakan Sudah Terdaftar")))
+        }});
+    }else{
       // field blank
-      CoolAlert.show(
-        context: context,
-        title: "Field Blank",
-        type: CoolAlertType.error,
-        text: "Please enter your email & password!",
-      );
+      ScaffoldMessenger.of(context)
+      .showSnackBar(SnackBar(content: Text("Isi Form Dengan Benar")));
+    }
+  }
+
+  //CekValidasi
+  checkValidation(GlobalKey<FormState> _formKey){
+    if (_formKey.currentState!.validate()){
+      print('Validated');
+      int aaaaaa = 1;
+    }else{
+      print('Not Validated');
     }
   }
 }
